0fb121177d023a6baeafa292a4c56e0ec1252691
usuario <usuario@10.22.146.207>
2013-11-29 12:23:28 +0000

    First commit

--- src/cataoracle/CataOracle.java
+++ src/cataoracle/CataOracle.java
@@ -8,7 +8,12 @@ import oracle.jdbc.OracleDriver;
 public class CataOracle {
 
     public static void main(String[] args) throws SQLException {
+        String userName = "system";
+        String password = "orcl";
+        String server = "localhost:100";
         DriverManager.registerDriver(new OracleDriver());
-        Connection connection = DriverManager.getConnection("jdbc:orcl:localhost:100");
+        // jdbc:oracle:thin:@server,userName,password
+        Connection connection = DriverManager.getConnection
+                ("jdbc:oracle:thin:@" + server, userName, password);
     }
 }
