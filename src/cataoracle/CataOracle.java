d2ba222b12e18a9a1bb48fffd3dd9db756d3cdf4
usuario <usuario@10.22.146.207>
2013-11-29 12:38:13 +0000

    First commit

--- src/cataoracle/CataOracle.java
+++ src/cataoracle/CataOracle.java
@@ -3,17 +3,26 @@ package cataoracle;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.naming.spi.DirStateFactory.Result;
 import oracle.jdbc.OracleDriver;
 public class CataOracle {
 
     public static void main(String[] args) throws SQLException {
         String userName = "system";
         String password = "orcl";
-        String server = "localhost:100";
+        String server = "localhost:100/orcl";
         DriverManager.registerDriver(new OracleDriver());
         // jdbc:oracle:thin:@server,userName,password
         Connection connection = DriverManager.getConnection
                 ("jdbc:oracle:thin:@" + server, userName, password);
+        Statement statement = connection.createStatement();
+        ResultSet set = statement.executeQuery("select * from CAMBIO_EUR_A_");
+        while(set.next()){
+            printRegister(set);
+        }        
     }
+    private static void printRegister(Result)
 }
